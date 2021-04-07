36cf085f0a98af7d8422e09ffe1ec27f1a5ae95e
JovicAbengona <37503727+JovicAbengona@users.noreply.github.com>
2021-04-07 16:20:19 +0800

    First push for CI Time Display

---
+++ application/views/index.php
@@ -0,0 +1,18 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="<?= base_url() ?>static/style.css">
+    <title>CodeIgniter | Time Display</title>
+</head>
+<body>
+    <header>
+        <h1>The Current Time and Date</h1>
+    </header>
+    <section>
+        <div><?= $date ?></div>
+    </section>
+</body>
+</html>
